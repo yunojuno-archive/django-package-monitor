91cc7551d5437097883b504260e2100033fbfd0d
Hugo Rodger-Brown <hugo@yunojuno.com>
2015-12-06 10:19:44 +0000

    Bump version to 0.2.3
    Fixes issues #1, #2, #3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.2.2",
+    version="0.2.3",
     packages=[
         'package_monitor',
         'package_monitor.management',
