98cf0fda3a018587dcfa99d2e4c01445a7e2e815
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-01-09 16:47:09 +0000

    Add all future versions to admin edit view

--- package_monitor/admin.py
+++ package_monitor/admin.py
@@ -1,17 +1,22 @@
 # -*- coding: utf8 -*-
-# import json
 import logging
 
-# from django.http import HttpResponseRedirect
 from django.contrib import admin
-# from django.contrib.admin import ModelAdmin
-# from django.core.urlresolvers import reverse
-# from django.utils.safestring import mark_safe
 from django.template.defaultfilters import truncatechars
+from django.utils.safestring import mark_safe
 
 from package_monitor.models import PackageVersion
 
 logger = logging.getLogger(__name__)
+
+
+def html_list(data):
+    """Convert dict into formatted HTML."""
+    if data is None:
+        return None
+    as_li = lambda v: "<li>%s</li>" % v
+    items = [as_li(v) for v in data]
+    return mark_safe("<ul>%s</ul>" % ''.join(items))
 
 
 def check_pypi(modeladmin, request, queryset):
@@ -37,12 +42,19 @@ class PackageVersionAdmin(admin.ModelAdmin):
     readonly_fields = (
         'package_name', 'is_editable', 'current_version', 'next_version',
         'latest_version', 'diff_status', 'checked_pypi_at',
-        'url', 'licence', 'raw'
+        'url', 'licence', 'raw', 'available_updates'
     )
 
     def _licence(self, obj):
         """Return truncated version of licence."""
         return truncatechars(obj.licence, 20)
     _licence.short_description = "PyPI licence"
+
+    def available_updates(self, obj):
+        """Print out all versions ahead of the current one."""
+        from package_monitor import pypi
+        package = pypi.Package(obj.package_name)
+        versions = package.all_versions()
+        return html_list([v for v in versions if v > obj.current_version])
 
 admin.site.register(PackageVersion, PackageVersionAdmin)
