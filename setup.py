6a9cfd1bcf7be34e0ac4e0e76bbd1b90d0e3b77b
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-01-09 18:36:19 +0000

    Bump version to 0.3.1
    Added list filter to admin.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.3.0",
+    version="0.3.1",
     packages=[
         'package_monitor',
         'package_monitor.management',
