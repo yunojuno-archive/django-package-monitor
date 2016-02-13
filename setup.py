f8fb6bc86316605328aeebdc5882769f5f852e87
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-02-13 12:48:33 +0000

    Bump version to 0.3.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.3.3",
+    version="0.3.4",
     packages=[
         'package_monitor',
         'package_monitor.management',
