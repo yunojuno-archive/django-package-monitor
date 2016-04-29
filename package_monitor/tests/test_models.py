10f60165944c8234cedf11048ce7c5dd2e2afa31
Hugo Rodger-Brown <hugo@yunojuno.com>
2016-04-29 14:11:06 +0100

    Add support for unparseable package versions.
    This commit adds a ne field (is_parseable) to the PackageVersion model,
    which is False if the version specified cannot be parsed by the
    semantic_version.Version.coerce method.

--- package_monitor/tests/test_models.py
+++ package_monitor/tests/test_models.py
@@ -23,6 +23,7 @@ class PackageVersionTests(TestCase):
         self.assertEqual(v.diff_status, 'unknown')
         self.assertEqual(v.checked_pypi_at, None)
         self.assertEqual(v.is_editable, False)
+        self.assertEqual(v.is_parseable, False)
         self.assertEqual(v.url, None)
 
         v.save()
@@ -34,6 +35,7 @@ class PackageVersionTests(TestCase):
         self.assertEqual(v.diff_status, 'unknown')
         self.assertEqual(v.checked_pypi_at, None)
         self.assertEqual(v.is_editable, False)
+        self.assertEqual(v.is_parseable, False)
         self.assertEqual(v.url, None)
 
         v.raw = 'foobar==1.2'
@@ -56,7 +58,6 @@ class PackageVersionTests(TestCase):
     def test_init_editable(self):
         url = "git+https://foobar.com#egg=foo"
         r = requirement.Requirement.parse("-e " + url)
-        print r.uri
         v = models.PackageVersion(requirement=r)
         self.assertEqual(v.package_name, 'foo')
         self.assertEqual(v.raw, '-e ' + url)
@@ -67,6 +68,19 @@ class PackageVersionTests(TestCase):
         self.assertEqual(v.checked_pypi_at, None)
         self.assertEqual(v.is_editable, True)
         self.assertEqual(v.url, '')
+
+    def test_init_unparseable(self):
+        r = requirement.Requirement.parse("foo==0.01.0")
+        v = models.PackageVersion(requirement=r)
+        self.assertEqual(v.package_name, 'foo')
+        self.assertEqual(v.current_version, None)
+        self.assertEqual(v.latest_version, None)
+        self.assertEqual(v.licence, '')
+        self.assertEqual(v.diff_status, 'unknown')
+        self.assertEqual(v.checked_pypi_at, None)
+        self.assertEqual(v.is_editable, False)
+        self.assertEqual(v.is_editable, False)
+        self.assertEqual(v.url, u'http://pypi.python.org/pypi/foo/json')
 
     @patch('requests.get', mock_get)
     def test_update_from_pypi(self):
@@ -79,3 +93,15 @@ class PackageVersionTests(TestCase):
         self.assertEqual(v.current_version, Version('0.0.1'))
         self.assertEqual(v.latest_version, Version('1.9.1'))
         self.assertEqual(v.diff_status, 'major')
+
+    @patch('requests.get', mock_get)
+    def test_update_from_pypi_unparseable(self):
+        """Test the update_from_pypi method for unparseable requirements."""
+        # editable packages return None
+        r = requirement.Requirement.parse("foobar==0.00.1")
+        v = models.PackageVersion(requirement=r)
+        v.update_from_pypi()
+        self.assertEqual(v.licence, 'BSD')
+        self.assertEqual(v.current_version, None)
+        self.assertEqual(v.latest_version, None)
+        self.assertEqual(v.diff_status, 'unknown')
