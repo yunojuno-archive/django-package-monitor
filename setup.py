528f68e7fd5a6aa7ede15c13b363cdc2d8871d19
Hugo Rodger-Brown <hugo@rodger-brown.com>
2016-11-22 16:40:00 +0000

    Bump version to v0.3.7

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.3.6",
+    version="0.3.7",
     packages=find_packages(),
     install_requires=REQUIREMENTS,
     include_package_data=True,
