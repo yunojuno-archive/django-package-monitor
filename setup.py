5c921c34570a3730606213d9896632c74181c18f
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-04-09 10:42:45 +0100

    Restore requirements.txt for testing purposes

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.5.1",
+    version="0.5.2",
     packages=find_packages(),
     install_requires=[
         'django>=1.8',
