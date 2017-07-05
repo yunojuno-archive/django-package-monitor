4927312e420fa39bd1e21304e3cc8b4a00438cc4
Hugo Rodger-Brown <hugo@yunojuno.com>
2017-07-05 17:26:19 +0100

    Bump version to v0.5.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
 
 setup(
     name="django-package-monitor",
-    version="0.5.5",
+    version="0.5.6",
     packages=find_packages(),
     install_requires=[
         'django>=1.8',
