34bec67fbf9cd1fa87037845a33ccd76c5ed735b
Hugo Rodger-Brown <hugo@yunojuno.com>
2020-09-12 16:12:06 +0100

    Update package classifiers

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from os import path, pardir, chdir
-from setuptools import setup, find_packages
+from os import chdir, pardir, path
 
-README = open(path.join(path.dirname(__file__), 'README.rst')).read()
+from setuptools import find_packages, setup
+
+README = open(path.join(path.dirname(__file__), "README.rst")).read()
 
 # allow setup.py to be run from any path
 chdir(path.normpath(path.join(path.abspath(__file__), pardir)))
@@ -11,31 +12,35 @@ setup(
     version="0.6.1",
     packages=find_packages(),
     install_requires=[
-        'django>=1.11',
-        'requests>=2.0',
-        'requirements_parser==0.1.0',
-        'semantic_version>=2.5',
+        "django>=1.11",
+        "requests>=2.0",
+        "requirements_parser==0.1.0",
+        "semantic_version>=2.5",
     ],
     include_package_data=True,
-    description='Requirements package monitor for Django projects.',
-    license='MIT',
+    description="Requirements package monitor for Django projects.",
+    license="MIT",
     long_description=README,
-    url='https://github.com/yunojuno/django-package-monitor',
-    author='YunoJuno',
-    author_email='code@yunojuno.com',
-    maintainer='YunoJuno',
-    maintainer_email='cod@yunojuno.com',
+    url="https://github.com/yunojuno/django-package-monitor",
+    author="YunoJuno",
+    author_email="code@yunojuno.com",
+    maintainer="YunoJuno",
+    maintainer_email="cod@yunojuno.com",
     classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3 :: only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
